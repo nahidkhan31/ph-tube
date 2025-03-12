d63f43b5e88f26a3f57b7816311ca864e03a0ae4
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-03-12 14:56:50 +0600

    added code of fetch section

---
+++ script/index.js
@@ -0,0 +1,26 @@
+function loadCategories() {
+  //   1-fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    // 2-convert promise to json
+    .then((res) => res.json())
+    // sent data to display
+    .then((data) => displayCategories(data.categories));
+}
+
+function displayCategories(categories) {
+  //   get the conteiner
+  const categoryConteiner = document.getElementById("category-container");
+
+  // Loop on Array of ob jects
+  for (let cat of categories) {
+    // create an Element
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+   <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+   `;
+
+    // Append the Element
+    categoryConteiner.append(categoryDiv);
+  }
+}
+loadCategories();
